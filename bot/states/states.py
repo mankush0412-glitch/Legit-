3900106ec93d233e9c868c03cc9c33a132f5c2fc
mankush0412-glitch <mankush0412@gmail.com>
2026-06-02 10:25:19 +0530

    Add files via upload

---
+++ bot/states/states.py
@@ -0,0 +1,55 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class DepositUPI(StatesGroup):
+    waiting_amount = State()
+    waiting_utr = State()
+
+
+class DepositCrypto(StatesGroup):
+    waiting_amount = State()
+    waiting_txhash = State()
+
+
+class BuyAccount(StatesGroup):
+    select_country = State()
+    confirm = State()
+    waiting_otp = State()
+
+
+class BulkBuyState(StatesGroup):
+    waiting_qty = State()
+
+
+class LoadSessionState(StatesGroup):
+    waiting_file = State()
+
+
+class AdminStates(StatesGroup):
+    upload_zip = State()
+    waiting_zip_password = State()
+    set_price = State()
+    set_price_value = State()
+    set_bulk_price = State()
+    broadcast = State()
+    broadcast_confirm = State()
+    add_balance_user = State()
+    add_balance_amount = State()
+    set_upi = State()
+    set_upi_name = State()
+    set_usdt = State()
+    set_support = State()
+    set_referral_bonus = State()
+    set_referral_percent = State()
+    set_bot_name = State()
+    set_ping_url = State()
+    add_country = State()
+    add_admin = State()
+    remove_admin = State()
+    ban_user = State()
+    search_user = State()
+    set_min_deposit = State()
+
+
+class SupportState(StatesGroup):
+    waiting_message = State()
